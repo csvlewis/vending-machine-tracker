ba0491ffe01e57b50608741810548ffadf16ccd3
Chris Lewis <13chlewis@gmail.com>
2019-02-15 10:45:18 -0700

    User Story 2 complete

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ describe Machine, type: :model do
     it { should have_many :snacks }
     it { should belong_to :owner }
   end
+
+  describe 'instance methods' do
+    describe '.average_price' do
+      it 'returns the average price for all items in a machine' do
+        owner = Owner.create(name: 'Jeff')
+        machine = Machine.create(location: 'Denver', owner: owner)
+        machine.snacks.create(name: 'Cheesy Poofs', price: 2.00)
+        machine.snacks.create(name: 'Bugles', price: 1.00)
+
+        expect(machine.average_price).to eq(1.50)
+      end
+    end
+  end
 end
